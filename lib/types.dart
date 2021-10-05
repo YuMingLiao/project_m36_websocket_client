4411cb585bed237109be9277b6dd2ce42395d862
YuMingLiao <dycan0716@gmail.com>
2021-10-05 04:48:48 +0000

    A freezed two-propertyclass(product type) works.

--- lib/types.dart
+++ lib/types.dart
@@ -1,8 +1,20 @@
 import 'dart:convert';
 import 'dart:core';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'types.freezed.dart';
 
 void main() {
   var json = '''{"promptInfo":{"schemaname":"main","headname":"master"}}''';
   Map<String, dynamic> user = jsonDecode(json);
   user.forEach((key, value) => print('${key}: ${value}'));
+
+  var promptInfo = PromptInfo('main', 'master');
+  print(promptInfo.schemaname);
+  print(promptInfo.headname); 
+}
+
+@freezed
+class PromptInfo with _$PromptInfo {
+  factory PromptInfo(String schemaname, String headname) = _PromptInfo;
 }
