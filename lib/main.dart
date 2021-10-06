aaa7928fcd2e957230b35448dafef0af2da5ad43
YuMingLiao <dycan0716@gmail.com>
2021-10-06 08:49:26 +0000

    WIP: 1. Obx shows schema:(null). 2. type error: String vs _JsonMap. guessing redundant fromJson. fix: 1. obs needs .value == null 2. jsonDecode creates nested map 3. list.insert(0,_) replaces reverse to make latest message stays top. 4. messages is clearer after fromJson
    takeaway:
    Don't mix Obx with StreamBuilder. Make one single data flow. Otherwise, I think, widget tree drawing would be affected.
    websocket_channel have listen to change Obx data.
    
    feature request:
    1. SelectableText to copy displayrelation json and analyze it.
    2. displayrelation's fromJson: need Atom, AtomType, Tuple, Attribute, Relation class.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:get/get.dart';
 import 'dart:convert';
 import 'behavior.dart';
 import 'package:pretty_json/pretty_json.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -22,6 +23,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -32,7 +34,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     final MyHomePageLogic _logic = Get.put(MyHomePageLogic());
@@ -45,30 +46,26 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Obx(() => Text(_logic.promptInfo.value == null
+                ? 'no db yet'
+                : 'Current Branch:(${_logic.promptInfo.value?.headName}) Schema:(${_logic.promptInfo.value?.schemaName})')),
             Form(
-                  child: TextFormField(
-                    controller: _logic.textCtr,
-                    decoration:
-                        const InputDecoration(labelText: 'enter a db name'),
-                  ),
-                ),
+              child: TextFormField(
+                controller: _logic.textCtr,
+                decoration: const InputDecoration(labelText: 'enter a db name'),
+              ),
+            ),
             Container(
               height: 250,
-              child: StreamBuilder(
-                stream: _logic._channel.stream,
-                builder: (context, snapshot) {
-                  _logic.handleResponse(snapshot);
-                  return ListView.builder(
-                    reverse: true,
+              child: Obx(() => ListView.builder(
+                    //reverse: true,
                     itemBuilder: (context, index) {
                       return ListTile(
                         title: Text(_logic.responses[index]),
                       );
                     },
                     itemCount: _logic.responses.length,
-                  );
-                },
-              ),
+                  )),
             ),
           ],
         ),
@@ -80,7 +77,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-
 
   @override
   void dispose() {
@@ -95,9 +91,13 @@ class MyHomePageLogic extends GetxController {
   final TextEditingController textCtr = TextEditingController();
   var connPhase = ConnectionPhase.connectdb.obs;
   var db = ''.obs;
-  var schemaname = ''.obs;
-  var headname = ''.obs;
+  var promptInfo = Rxn<PromptInfo>(); //nullable observable variable
+  var displayError = Rxn<DisplayError>();
   var responses = [].obs;
+
+  void onInit() {
+    _channel.stream.listen((msg) => handleResponse(msg));
+  }
 
   void sendMessage() {
     print('send ${textCtr.text}');
@@ -117,18 +117,68 @@ class MyHomePageLogic extends GetxController {
     }
   }
 
-  void handleResponse(snapshot){
-    if (snapshot.hasData) { 
-      Map<String, dynamic> json = jsonDecode(snapshot.data.toString());
-// unexpected response: promptInfo. expected displayrelation in :showexpr true, a := ... :showexpr a, ...
-//TODO: displayerror, displayrelation, promptInfo
-
-      //TODO: change state of PromptInfo
-      responses.add(prettyJson(json, indent: 2));
+  void handleResponse(msg) {
+    Map<String, dynamic> json = jsonDecode(msg);
+    if (json.containsKey('promptInfo')) {
+      promptInfo.value = PromptInfo.fromJson(json['promptInfo']);
+    } else if (json.containsKey('displayerror')) {
+      displayError.value = DisplayError.fromJson(json['displayerror']);
+      responses.insert(0, displayError.value?.error);
+    }
+    else if (json.containsKey('displayrelation')){
+      responses.insert(0, prettyJson(json, indent: 2));
     }
   }
-  void onClose(){
+
+  void onClose() {
     _channel.sink.close();
   }
-
 }
+
+class PromptInfo {
+  final String schemaName;
+  final String headName;
+
+  PromptInfo(this.schemaName, this.headName);
+
+  PromptInfo.fromJson(Map<String, dynamic> json)
+      : schemaName = json['schemaname'],
+        headName = json['headname'];
+
+  Map<String, dynamic> toJson() => {
+        'promptInfo': {
+          'schemaname': schemaName,
+          'headname': headName,
+        }
+      };
+}
+
+class DisplayError {
+  final String error;
+
+  DisplayError(this.error);
+
+  DisplayError.fromJson(Map<String, dynamic> json) : error = json['json'];
+
+  Map<String, dynamic> toJson() => {
+        'displayerror': {'json': error}
+      };
+}
+
+/*
+class DisplayRelation {
+
+  DisplayRelation();
+
+  DisplayRelation.fromJson(Map<String, dynamic> json)
+      : schemaName = json['schemaname'],
+        headName = json['headname'];
+
+  Map<String, dynamic> toJson() => {
+        'displayrelation': {
+          'schemaname': schemaName,
+          'headname': headName,
+        }
+      };
+}
+*/
